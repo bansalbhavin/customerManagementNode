1ff82ac0ca84b82ebca725da6127df96bfcf7489
bansalbhavin <bansalbhavin@gmail.com>
2017-04-19 17:37:26 +0530

    Change in server file for port message.

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.get('*', routes.index);
 
 // Start server
 
-app.listen(3000, function () {
-    console.log("CustMgr Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(3001, function () {
+    console.log("Customer manager Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
